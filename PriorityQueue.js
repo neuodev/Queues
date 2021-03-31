e57722fdbbde40d5347b1df1a753ba5cb8796245
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 12:24:00 +0200

    Add insert method to the prioriy Queue

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -2,6 +2,30 @@ class PriorityQueue {
   constructor() {
     this.priorityQueue = [];
   }
+
+  insert(val) {
+    const lastIdx = this.priorityQueue.length - 1;
+    if (!val) return;
+    if (this.priorityQueue.length === 0) this.priorityQueue.push(val);
+    else if (this.priorityQueue[lastIdx] < val) {
+      this.priorityQueue.push(val);
+    } else {
+      let i;
+      for (i = lastIdx; i >= 0; i--) {
+        if (this.priorityQueue[i] > val) {
+          this.priorityQueue[i + 1] = this.priorityQueue[i];
+        } else {
+          break;
+        }
+      }
+      this.priorityQueue[i + 1] = val;
+    }
+  }
 }
 
 const priorityQueue = new PriorityQueue();
+priorityQueue.insert(5);
+priorityQueue.insert(3);
+priorityQueue.insert(6);
+
+console.log(priorityQueue);
