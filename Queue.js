ee4584515c2a925363f2bfe3a4a2fcddf06c7272
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 11:19:12 +0200

    Add the peek  method to the Queue class

--- Queue.js
+++ Queue.js
@@ -16,10 +16,12 @@ class Queue {
   }
   dequeue() {
     if (this.array.length === 0) return false;
-    if (this.array.length - 1 === this.F) return 
+    if (this.array.length  === this.F) return;
     this.F = this.F + 1;
   }
-
+  peek() {
+    return this.array[this.F];
+  }
 }
 
 const queue = new Queue();
@@ -28,6 +30,11 @@ queue.enqueue(20);
 queue.enqueue(30);
 queue.enqueue(40);
 queue.enqueue(50);
-queue.dequeue();
-queue.dequeue();
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+console.log(queue.peek());
 console.log(queue);
