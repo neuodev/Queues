e9e9ca405cddd412d1aefd7b9753e8a5d88cf8e6
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 11:52:51 +0200

    Building  a queue using a two stacks

---
+++ Building_a_Queue_using_Stacks.js
@@ -0,0 +1,30 @@
+class Queue {
+  constructor() {
+    this.S1 = [];
+    this.S2 = [];
+  }
+
+  enqueue(val) {
+    if (!val) return;
+    this.S1.push(val);
+  }
+  dequeue() {
+    if (this.S2.length === 0) {
+      while (this.S1.length !== 0) {
+        const lastElement = this.S1.pop();
+        this.S2.push(lastElement);
+      }
+    }
+    return this.S2.pop();
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue);
